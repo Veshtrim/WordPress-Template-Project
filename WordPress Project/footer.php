ab78701d6d1700b6d5695196edd117723811fe53
Vështrim <veshtrimm1@gmail.com>
2022-11-14 20:40:36 +0100

    commit

---
+++ WordPress Project/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+    <p class="copyright">Football News-Copyright 2022</p>
+          
+				<?php 
+			  	dynamic_sidebar('footer-1');
+			   	?>
+</footer>
+   
+   </div>
+	<?php
+		wp_footer();
+	?>
+    <!-- Bootstrap Javascript --> 
+ 
+</body>
+</html> 
